1086527534d03d3c710c5c8635141183367d70e9
imad-touil <imadtouil.it@gmail.com>
2025-05-30 20:15:36 +0100

    update echo

--- builts0.c
+++ builts0.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builts.c                                           :+:      :+:    :+:   */
+/*   builts0.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 12:53:17 by imatouil          #+#    #+#             */
-/*   Updated: 2025/05/30 17:15:11 by imatouil         ###   ########.fr       */
+/*   Updated: 2025/05/30 19:21:30 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,24 @@
 void	ft_echo(t_command *commands)
 {
 	int	i;
+	int	key;
 	int	newline;
 
-	i = 0;
-	newline = 1;
-	if (commands->args[1] && !ft_strncmp(commands->args[1], "-n", 2))
+	newline = 0;
+	i = 1;
+	key = 0;
+	while (commands->args[1][++i])
 	{
-		newline = 0;
-		i++;
+		if (commands->args[1][i] != 'n')
+			key = 1;
 	}
+	if (!ft_strncmp(commands->args[1], "-n", 2) && key)
+	{
+		newline = 1;
+		i = 0;
+	}
+	else
+		i = 1;
 	while (commands->args[++i])
 	{
 		printf("%s", commands->args[i]);
